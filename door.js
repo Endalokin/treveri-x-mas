a513d0b8d8f9bee7c6d4e5a10a9e8960230730b8
Nora Westphal <tupfen@hotmail.de>
2023-11-20 20:04:08 +0100

    have everything running on door

---
+++ door.js
@@ -0,0 +1,36 @@
+var elements = document.querySelectorAll(".door");
+const today = new Date();
+
+for (let element of elements) {
+  element.addEventListener("click", toggleDoor);
+}
+
+function toggleDoor() {
+  var parent = this.parentElement;
+  let currentDay = days.find((d) => {
+    return d.id == parent.id;
+  });
+  const thisDay = currentDay.day;
+  if (thisDay > today.getDate()) {
+    alert("Du musst dich noch ein wenig gedulden!");
+  } else {
+    this.classList.toggle("doorOpen");
+    let imgUrl = currentDay.img;
+    let linkUrl = currentDay.link;
+    if (this.classList.value.match("doorOpen")) {
+      setTimeout(() => {
+        parent.style.backgroundImage = `url(${imgUrl})`;
+      }, 100);
+      setTimeout(() => {
+        parent.parentElement.setAttribute("href", linkUrl);
+        parent.parentElement.setAttribute("target", "_blank");
+      }, 400);
+    } else {
+      parent.parentElement.setAttribute("href", "#");
+      parent.parentElement.setAttribute("target", "");
+      setTimeout(() => {
+        parent.style.backgroundImage = null;
+      }, 100);
+    }
+  }
+}
