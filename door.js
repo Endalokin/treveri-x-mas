f7e76378a854317cf2a78e64466ff8e4b5a86b33
Nora Westphal <tupfen@hotmail.de>
2023-11-25 19:26:54 +0100

    unused stuff deleted

--- door.js
+++ door.js
@@ -3,6 +3,25 @@ const today = new Date();
 
 for (let element of elements) {
   element.addEventListener("click", toggleDoor);
+  element.addEventListener("mouseenter", sneakDoor);
+  element.addEventListener("mouseleave", sneakDoor);
+}
+
+function checkDay(currentDay, sendAlert = false) {
+  const thisDay = currentDay.day;
+  if (thisDay > today.getDate()) {
+    sendAlert && alert("Du musst dich noch ein wenig gedulden!");
+    return true;
+  }
+}
+
+function sneakDoor() {
+  var parent = this.parentElement;
+  let currentDay = days.find((d) => {
+    return d.id == parent.id;
+  });
+  if (checkDay(currentDay)) return;
+  this.classList.toggle("openable");
 }
 
 function toggleDoor() {
@@ -10,32 +29,27 @@ function toggleDoor() {
   let currentDay = days.find((d) => {
     return d.id == parent.id;
   });
-  const thisDay = currentDay.day;
-  if (thisDay > today.getDate()) {
-    alert("Du musst dich noch ein wenig gedulden!");
-  } else {
-    this.setAttribute("onmouseenter", "");
-    this.setAttribute("onmouseleave", "");
-    this.classList.toggle("openable");
-    this.classList.toggle("doorOpen");
+  if (checkDay(currentDay, true)) return;
+  this.removeEventListener("mouseenter", sneakDoor);
+  this.removeEventListener("mouseleave", sneakDoor);
+  this.classList.toggle("openable");
+  this.classList.toggle("doorOpen");
 
-    console.log(this);
-    let imgUrl = currentDay.img;
-    let linkUrl = currentDay.link;
-    if (this.classList.value.match("doorOpen")) {
-      setTimeout(() => {
-        parent.style.backgroundImage = `url(${imgUrl})`;
-      }, 100);
-      setTimeout(() => {
-        parent.parentElement.setAttribute("href", linkUrl);
-        parent.parentElement.setAttribute("target", "_blank");
-      }, 400);
-    } else {
-      parent.parentElement.setAttribute("href", "#");
-      parent.parentElement.setAttribute("target", "");
-      setTimeout(() => {
-        parent.style.backgroundImage = null;
-      }, 100);
-    }
+  let imgUrl = currentDay.img;
+  let linkUrl = currentDay.link;
+  if (this.classList.value.match("doorOpen")) {
+    setTimeout(() => {
+      parent.style.backgroundImage = `url(${imgUrl})`;
+    }, 100);
+    setTimeout(() => {
+      parent.parentElement.setAttribute("href", linkUrl);
+      parent.parentElement.setAttribute("target", "_blank");
+    }, 400);
+  } else {
+    parent.parentElement.setAttribute("href", "#");
+    parent.parentElement.setAttribute("target", "");
+    setTimeout(() => {
+      parent.style.backgroundImage = null;
+    }, 100);
   }
 }
